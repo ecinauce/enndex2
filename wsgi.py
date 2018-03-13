e6b34c3f740bd4005190bdef8801a615f08a1836
ecinauce <nicololynsuecuacion@gmail.com>
2018-03-13 16:23:19 +0800

    [BUGFIX] Holy shit, I actually had to dig deep to figure out what went fucking wrong.

--- wsgi.py
+++ wsgi.py
@@ -19,7 +19,7 @@ def ball():
 def index():
   from pymongo import MongoClient
   
-  wew = MongoClient('mongodb://userUIA:8YhwyYPaUmfH4Khx@mongodb/ennchandb')
+  wew = MongoClient('mongodb://userUIA:8YhwyYPaUmfH4Khx@mongodb:27017/ennchandb')
   
   wewDb = wew.db
   wewCol = wewDb.col
